f8a1c803c0703ec3fd94a5dc547d6b7f628fbdea
monirol4664-glitch <monirol4664@gmail.com>
2026-04-09 07:52:44 +0600

    update

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE students (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    full_name TEXT,
-    major TEXT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
